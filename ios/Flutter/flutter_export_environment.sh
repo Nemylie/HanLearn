d9bda0e22bbaad45b2d5b7f6efb4c9fdd3b06ff8
azwa <azwanurnisya612@gmail.com>
2026-01-18 06:49:39 +0800

    test

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\flutt\OneDrive\Documents\hanlearn"
+export "FLUTTER_ROOT=C:\Users\AZWA\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\AZWA\OneDrive\Documents\HanLearn"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
